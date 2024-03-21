01ad4d8983d0f1ef8686316914664d4b57246095
Julien Schueller <schueller@phimeca.com>
2024-03-21 07:54:55 +0100

    CMake: Drop expout files

--- python/test/t_docstring.py
+++ python/test/t_docstring.py
@@ -3,12 +3,13 @@
 import ottemplate
 import os
 import shutil
+import sys
 from glob import glob
 from tempfile import mkdtemp
 import doctest
 
 mod_path = os.path.dirname(ottemplate.__file__)
-py_files = sorted(glob(mod_path + '/*.py'))
+py_files = sorted(glob(mod_path + "/*.py"))
 
 total_failure_count = 0
 total_test_count = 0
@@ -19,20 +20,22 @@ os.chdir(work_dir)
 
 for py_file in py_files:
     py_basename = os.path.splitext(os.path.basename(py_file))[0]
-    module = __import__('ottemplate.' + py_basename, fromlist=[py_basename])
+    module = __import__("ottemplate." + py_basename, fromlist=[py_basename])
     failure_count, test_count = doctest.testmod(
         module, verbose=False, optionflags=doctest.ELLIPSIS)
 
     total_failure_count += failure_count
     total_test_count += test_count
 
-    print(('%s %5d tests failed'
-           % ((py_basename + ' ').ljust(60, '.'), failure_count)))
+    print(("%s %5d tests failed"
+           % ((py_basename + " ").ljust(60, "."), failure_count)))
 
-print(('-' * 79))
-print(('%s %5d tests failed'
-       % ('TOTAL '.ljust(60, '.'), total_failure_count)))
+print(("-" * 79))
+print(("%s %5d tests failed"
+       % ("TOTAL ".ljust(60, "."), total_failure_count)))
 
 # Delete temporary working directory for IO tests
-os.chdir('..')
+os.chdir("..")
 shutil.rmtree(work_dir)
+
+sys.exit(total_failure_count)
