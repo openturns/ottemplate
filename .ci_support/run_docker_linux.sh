f38e5a08759adc40beadb0fd6400c39c1762acb7
Julien Schueller <schueller@phimeca.com>
2025-09-12 16:34:10 +0200

    CI: Add -Wsuggest-override

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -5,7 +5,7 @@ set -xe
 cd /tmp
 cmake -DCMAKE_INSTALL_PREFIX=${HOME}/.local \
       -DCMAKE_UNITY_BUILD=ON \
-      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
+      -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Wsuggest-override -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
       -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter" \
       -DUSE_SPHINX=ON -DSPHINX_FLAGS="-W -T -j4" \
       -B build /io
