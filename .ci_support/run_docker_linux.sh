9903cba41bd059d5fc7c3811a3f01b2243e6fdab
Julien Schueller <schueller@phimeca.com>
2026-02-13 13:04:52 +0100

    CI: Fix coverage

--- .ci_support/run_docker_linux.sh
+++ .ci_support/run_docker_linux.sh
@@ -6,7 +6,7 @@ cd /tmp
 cmake -DCMAKE_INSTALL_PREFIX=${HOME}/.local \
       -DCMAKE_UNITY_BUILD=ON \
       -DCMAKE_CXX_FLAGS="-Wall -Wextra -Wpedantic -Wshadow -Wsuggest-override -Werror -D_GLIBCXX_ASSERTIONS --coverage" \
-      -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter" \
+      -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter -Wno-shadow" \
       -DUSE_SPHINX=ON -DSPHINX_FLAGS="-W -T -j4" \
       -B build /io
 cd build
@@ -16,7 +16,7 @@ ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
 
 # coverage
 gcov `find lib/src/ -name "*.gcno"`
-lcov --capture --directory . --output-file coverage.info --include "*.cxx"
+lcov --capture --directory lib --output-file coverage.info --include "*.cxx" -j 4
 genhtml --output-directory coverage coverage.info
 cp -v coverage.info coverage
 
